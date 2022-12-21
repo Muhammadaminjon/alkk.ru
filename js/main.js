534775927736ecedb16110eaae0d75c88672e87f
Muhammadaminjon <muhammadaminovmuhammadamin303@gmail.com>
2022-12-21 23:32:14 +0500

    ani

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+window.addEventListener("load", () => {
+  gsap.from(".site-logo", {
+    x: -500,
+    opancity: 0,
+    duration: 1,
+  });
+  gsap.from(".header__nav__item", {
+    y: -200,
+    opancity: 0,
+    duration: 1,
+    delay: .5,
+    stagger: 0.35,
+  });
+  gsap.from(".header__top-wrapper", {
+    x: 500,
+    opancity: 0,
+    duration: 1,
+  });
+});
+
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".header");
 let xbtn = document.querySelector(".x-btn");
