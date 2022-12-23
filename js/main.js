2e54b89171af74ab15b07820c18de9cd559f285c
Muhammadaminjon <muhammadaminovmuhammadamin303@gmail.com>
2022-12-23 16:47:23 +0500

    new main section

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-window.addEventListener("load", () => {
-  gsap.from(".site-logo", {
-    x: -500,
-    opancity: 0,
-    duration: 1,
-  });
-  gsap.from(".header__nav__item", {
-    y: -200,
-    opancity: 0,
-    duration: 1,
-    delay: .5,
-    stagger: 0.35,
-  });
-  gsap.from(".header__top-wrapper", {
-    x: 500,
-    opancity: 0,
-    duration: 1,
-  });
-});
-
 let burger = document.querySelector(".burger");
 let menu = document.querySelector(".header");
 let xbtn = document.querySelector(".x-btn");
@@ -26,4 +6,29 @@ burger.addEventListener("click", () => {
 });
 xbtn.addEventListener("click", () => {
   menu.classList.remove("show");
+});
+
+const tl = gsap.timeline(5);
+
+window.addEventListener("load", () => {
+  tl.from(".site-logo", {
+    x: -700,
+    opacity: 0,
+    duration: 1.35,
+  });
+  tl.from(".header__top-wrapper", {
+    x: 700,
+    opacity: 0,
+    duration: 1.35,
+  });
+  tl.from(".header__nav__item", {
+    y: 200,
+    opacity: 0,
+    duration: 0.75,
+    stagger: 0.35,
+  });
+  tl.from(".header-nav__slash", {
+    duration: 0.35,
+    opacity: 0,
+  });
 });
